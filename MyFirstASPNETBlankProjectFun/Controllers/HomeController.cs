ad78d13dfff778be59f178e78f8a3d0f592ac073
elizatuinei <55069280+elizatuinei@users.noreply.github.com>
2024-02-07 10:14:35 -0700

    MVCchanges

---
+++ MyFirstASPNETBlankProjectFun/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyFirstASPNETBlankProjectFun.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Calculator()
+        {
+            // write our code here
+            return View();
+        }
+    }
+}
--- MyFirstASPNETBlankProjectFun/Program.cs
+++ MyFirstASPNETBlankProjectFun/Program.cs
@@ -1,6 +1,20 @@
 var builder = WebApplication.CreateBuilder(args);
+// set up services
+builder.Services.AddControllersWithViews();
 var app = builder.Build();
 
+// app.UseDefaultFiles();
+
+app.UseHttpsRedirection();
 app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}"
+    );
 
 app.Run();
